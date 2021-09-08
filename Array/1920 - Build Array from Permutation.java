d98ea473898b9c7498a9e4a8ea83da64ebc44bd5
Bhavik Patel <bhavikpatel7023@gmail.com>
2021-09-07 22:56:32 -0400

    Create 1920 - Build Array from Permutation.java

---
+++ Array/1920 - Build Array from Permutation.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] buildArray(int[] nums) {
+        
+        int[] returnArray = new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            returnArray[i] = nums[nums[i]];
+        }
+        
+        return returnArray;
+        
+    }
+}
